3ec66b3a56fa0a797fec9e687d37f6576f46d2db
Dustin <dustinroberts96@gmail.com>
2020-06-26 08:53:18 -0400

    Finished Cli

--- test.py
+++ test.py
@@ -2,19 +2,29 @@
 #Synxis Automation 6/25/2020
 import pyautogui, sys, time
 print('Press Ctrl-C to quit.')
+
 def main():
     try:
+      first = sys.argv[1]
+      last = sys.argv[2]
+      room  = sys.argv[3]
+
+      room_type = sys.argv[4]
+      name  = str(first + " " + last)
+      print(name, room, room_type)
+      
       pyautogui.FAILSAFE = False
-      pyautogui.click(x=1879, y=216)
+      pyautogui.click(x=1879, y=221)
   
       time.sleep(5)
-
-      pyautogui.click(x=739, y=585)
+      print("111")
+      pyautogui.click(x=713, y=585)
       
-      print("getrrrrrrrrrrrrrrrrrr")
-      time.sleep(3)
+      time.sleep(4)
       #input room
-      pyautogui.write('NK1', interval=0.25) 
+     
+      pyautogui.write(str(room_type), interval=0.25)
+      time.sleep(1)
       pyautogui.press('tab', presses=1)
       pyautogui.write('LT02', interval=0.25) 
       pyautogui.press('tab', presses=4)
@@ -27,7 +37,7 @@ def main():
       
       #enter name or wyndham number, need to implement a better soultion
       #maybe get a list of all vip members and copy folio
-      pyautogui.write('Mark Mcnally', interval=0.25)
+      pyautogui.write(name, interval=0.25)
       pyautogui.press('enter', presses=1)
       
       #Instead of time.sleep maybe wait until screen sees dialog box
@@ -55,10 +65,23 @@ def main():
       pyautogui.click(x=93, y=506)
       time.sleep(0.5)
       pyautogui.click(x=748, y=453)
+      pyautogui.press('tab', presses=2)
+      pyautogui.press('enter', presses=1)
+      time.sleep(1)
+
+      #room#
+      pyautogui.click(x=126, y=577)
+      time.sleep(1)
+      pyautogui.write(room, interval=0.25)
+
+  
       pyautogui.click(x=1054, y=664)
       time.sleep(1)
       pyautogui.click(x=1864, y=344)
       time.sleep(1)
+
+
+      
       pyautogui.press('enter', presses=1)
 
       
